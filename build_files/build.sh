2f2e248085187124d32b63376d1f5f1ab121be74
adamthiede <me@adamthiede.com>
2026-06-13 12:46:12 -0500

    update build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,27 +2,13 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
+# install lowlevel network tool, basic editor, and a thing I need for accessing media
+# remove firefox - use from flathub
 dnf5 install -y tailscale vim gvfs-nfs
-dnf5 remove -y firefox gnome-software
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable tailscaled.service
-systemctl enable rpm-ostreed-automatic.timer
+dnf5 remove -y firefox
 
 echo -e "[Daemon]\nAutomaticUpdatePolicy=stage" > /etc/rpm-ostreed.conf
+
+# enable tailscale and auto updates
+systemctl enable tailscaled.service
+systemctl enable rpm-ostreed-automatic.timer
